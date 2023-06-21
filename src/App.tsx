d8cc6a4e5d83abe1964e3039893c63649e01eacb
Luis Mario <luis.mario.trujillo@live.com.mx>
2023-06-21 17:20:27 -0600

    Added font and download file logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import { TradingCard, ApiResponse } from './types';
 import { groupByCardName } from './cardUtils';
-import { Card, Grid, CardContent, Typography, Button, Backdrop, CircularProgress } from '@mui/material';
+import { Card, Grid, CardContent, Typography, Backdrop, CircularProgress } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
 import AppBarComponent from './AppBarComponent';
+import { CSVLink } from 'react-csv';
+import * as xlsx from 'xlsx';
 
 function App() {
   const [data, setData] = useState<TradingCard[] | null>(null);
@@ -37,6 +39,60 @@ function App() {
     document.body.classList.toggle('dark-mode');
   };
 
+  const handleDownload = (format: string) => {
+    if (format === 'json') {
+      const jsonData = JSON.stringify(groupedCards);
+      const blob = new Blob([jsonData], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'groupedCards.json';
+      a.click();
+      URL.revokeObjectURL(url);
+    } else if (format === 'csv') {
+      const csvData = groupedCards.map(group => ({
+        'Card Name': group.cardName,
+        'Average Price': group.averagePrice.toFixed(2),
+        'Lower Bound': group.lowerBound.toFixed(2),
+        'Upper Bound': group.upperBound.toFixed(2),
+        'Standard Deviation': group.standardDeviation.toFixed(2),
+        'Peak Price': group.peakPrice.toFixed(2),
+        'Peak Day': group.peakDay
+      }));
+      const headers = Object.keys(csvData[0]);
+      const csv = [headers, ...csvData].map(row => headers.map(fieldName => row[fieldName]));
+      const csvContent = csv.map(row => row.join(',')).join('\n');
+      const blob = new Blob([csvContent], { type: 'text/csv' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'groupedCards.csv';
+      a.click();
+      URL.revokeObjectURL(url);
+    } else if (format === 'xlsx') {
+      const workbook = xlsx.utils.book_new();
+      const sheetData = groupedCards.map(group => ({
+        'Card Name': group.cardName,
+        'Average Price': group.averagePrice.toFixed(2),
+        'Lower Bound': group.lowerBound.toFixed(2),
+        'Upper Bound': group.upperBound.toFixed(2),
+        'Standard Deviation': group.standardDeviation.toFixed(2),
+        'Peak Price': group.peakPrice.toFixed(2),
+        'Peak Day': group.peakDay
+      }));
+      const worksheet = xlsx.utils.json_to_sheet(sheetData);
+      xlsx.utils.book_append_sheet(workbook, worksheet, 'groupedCards');
+      const xlsxBuffer = xlsx.write(workbook, { type: 'buffer', bookType: 'xlsx' });
+      const blob = new Blob([xlsxBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'groupedCards.xlsx';
+      a.click();
+      URL.revokeObjectURL(url);
+    }
+  };
+
   if (!data) {
     return (
       <Backdrop open>
@@ -53,7 +109,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <AppBarComponent darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <AppBarComponent darkMode={darkMode} toggleDarkMode={toggleDarkMode} handleDownload={handleDownload} />
       <div className={`app ${darkMode ? 'dark' : ''}`}>
         <Grid container spacing={2}>
           {groupedCards.map((group) => (
--- src/AppBarComponent.tsx
+++ src/AppBarComponent.tsx
@@ -1,29 +1,69 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Switch, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Switch, IconButton, Menu, MenuItem } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import { SaveAlt as SaveAltIcon } from '@mui/icons-material';
 
 interface AppBarProps {
   darkMode: boolean;
   toggleDarkMode: () => void;
+  handleDownload: (format: string) => void;
 }
 
-const CustomAppBar: React.FC<AppBarProps> = ({ darkMode, toggleDarkMode }) => {
+const CustomAppBar: React.FC<AppBarProps> = ({ darkMode, toggleDarkMode, handleDownload }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDownloadOption = (format: string) => {
+    handleDownload(format);
+    handleClose();
+  };
+
   return (
     <AppBar
       position="static"
       sx={{
-        backgroundColor: darkMode ?  '#c09c74' : '#302c2c' ,
-        color: darkMode ?   'black' : 'white'
+        backgroundColor: darkMode ? '#c09c74' : '#302c2c',
+        color: darkMode ? 'black' : 'white',
       }}
     >
       <Toolbar>
-        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-        MAGPIE Trading Cards
+        <Typography variant="h5" component="div" sx={{ flexGrow: 1, fontFamily: 'Neometric, sans-serif' }}>
+          <span style={{ marginRight: '8px' }}>MAGPIE Trading Cards</span>
+          <IconButton
+            color="inherit"
+            onClick={handleClick}
+            aria-controls="download-menu"
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <SaveAltIcon />
+          </IconButton>
+          <Menu
+            id="download-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'download-button',
+            }}
+          >
+            <MenuItem onClick={() => handleDownloadOption('json')}>Download as JSON</MenuItem>
+            <MenuItem onClick={() => handleDownloadOption('csv')}>Download as CSV</MenuItem>
+            <MenuItem onClick={() => handleDownloadOption('xlsx')}>Download as XLSX</MenuItem>
+          </Menu>
         </Typography>
-        <WbSunnyIcon sx={{ marginRight: "8px" }} />
+        <WbSunnyIcon sx={{ marginRight: '8px' }} />
         <Switch checked={darkMode} onChange={toggleDarkMode} color="default" />
-        <Brightness4Icon sx={{ marginLeft: "8px" }} />
+        <Brightness4Icon sx={{ marginLeft: '8px' }} />
       </Toolbar>
     </AppBar>
   );
